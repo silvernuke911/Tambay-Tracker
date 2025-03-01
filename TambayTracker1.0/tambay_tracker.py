6279ad2e01d9b56099fa470a2a661892f83b77f8
silvernuke911 <vercilbenben5@gmail.com>
2025-03-01 20:07:38 +0800

    'lol'

--- TambayTracker1.0/tambay_tracker.py
+++ TambayTracker1.0/tambay_tracker.py
@@ -4,7 +4,7 @@ from modules import query
 
 ## Something something, comment here on the purpose of this shit and shit like that, something something.
 
-# Data files
+# Data files path
 raw_data_file = r'Data\tambay_tracker_data.csv'
 date_file     = r'Data\date_list.csv'
 score_file    = r'Data\scores_list.csv'
@@ -12,15 +12,50 @@ member_file   = r'Data\member_list.csv'
 
 # Valid entries / options
 valid_names = validators.load_valid_names(member_file)
-program_options = ['Enter new entry','Show raw data', 'Update scores', 'Show Points', 'Show Point Order', 'Show Date Frequency', 'Show Attendance Proportion', 'Enter Special Points', 'Enter New Member','Exit']
+program_options = [
+    'Enter new entry',
+    'Show raw data', 
+    'Update scores', 
+    'Show Points', 
+    'Show Point Order', 
+    'Show Date Frequency', 
+    'Show Attendance Proportion', 
+    'Enter Special Points', 
+    'Enter New Member',
+    'Exit'
+]
 
 def main():
-    prompt = entry.starting_menu(raw_data_file, score_file, date_file, valid_names) 
+    prompt = entry.starting_menu(
+        raw_data_file, 
+        score_file, 
+        date_file, 
+        valid_names
+    ) 
     if prompt in ['y','']:
-        entry.get_entry_input(raw_data_file, score_file, date_file, valid_names)
+        entry.get_entry_input(
+            raw_data_file, 
+            score_file, 
+            date_file, 
+            valid_names
+        )
     while True:
-        option_choice = query.get_option_input(program_options, raw_data_file, score_file, date_file, valid_names)
-        entry.handle_option_choice(option_choice, raw_data_file, score_file, date_file, valid_names, member_file)
+        option_choice = query.get_option_input(
+            program_options, 
+            raw_data_file, 
+            score_file, 
+            date_file, 
+            valid_names
+        )
+        entry.handle_option_choice(
+            option_choice, 
+            raw_data_file, 
+            score_file, 
+            date_file, 
+            valid_names, 
+            member_file
+        )
 
+# Run main program 
 if __name__ == '__main__':
     main()
